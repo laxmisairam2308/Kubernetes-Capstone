1874b03aaa0066cf1784b8add5ec702ef8629a7e
laxmisairam2308 <108729203+laxmisairam2308@users.noreply.github.com>
2025-05-09 15:06:00 +0530

    Update setup.sh

--- deployment_environment/setup.sh
+++ deployment_environment/setup.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+# Install required tools
+sudo apt update && sudo apt install -y docker.io kubectl awscli eksctl
+# Enable Docker
+sudo systemctl start docker
+sudo systemctl enable docker
